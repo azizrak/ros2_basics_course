7af7db4f0b7a3ef7a23b9bf4a5b69c8f62718d92
azizrak <azizrak@student.ubc.ca>
2024-11-08 07:16:23 +0000

    unit 5 (incomplete) 07-11-2024

---
+++ exercise42_pkg/src/exercise42.cpp
@@ -0,0 +1,67 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_srvs/srv/set_bool.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+#include <memory>
+
+using Empty = std_srvs::srv::SetBool;
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+class ServerNode : public rclcpp::Node
+{
+public:
+  ServerNode()
+  : Node("service_right")
+  {
+
+    srv_ = create_service<Empty>("right", std::bind(&ServerNode::right_callback, this, _1, _2));
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+
+  }
+
+private:
+  rclcpp::Service<Empty>::SharedPtr srv_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+
+  void right_callback(
+      const std::shared_ptr<Empty::Request> request,
+      const std::shared_ptr<Empty::Response> response) 
+    {
+        auto message = geometry_msgs::msg::Twist();
+
+        if(request->data == true)
+        {   
+            // Send velocities to move the robot to the right
+            message.linear.x = 0.2;
+            message.angular.z = -0.2;
+            publisher_->publish(message);
+
+            // Set the response success variable to true
+            response->success = true;
+            // Set the response message variable to a string
+            response->message = "Turning to the right right right!";
+        }
+
+        if (request->data == false)
+        {
+            // Send velocities to stop the robot
+            message.linear.x = 0.0;
+            message.angular.z = 0.0;
+            publisher_->publish(message);
+
+            // Set the response success variable to false
+            response->success = false;
+            // Set the response message variable to a string
+            response->message = "It is time to stop!"; 
+        }    
+    }
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ServerNode>());
+  rclcpp::shutdown();
+  return 0;
+}
